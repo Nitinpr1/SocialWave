c26ebad68a44dd0448ae579e36fb2f461b4a9300
Nitinpr1 <nitin.pr2002@gmail.com>
2024-02-05 23:11:19 +0530

    first commit

---
+++ server/routes/post.js
@@ -0,0 +1,14 @@
+import express, { Router } from "express";
+import { getFeedPosts, getUserPosts, likePost } from "../controllers/post.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = new Router();
+
+//READ
+router.get("/", verifyToken, getFeedPosts);
+router.get("/:userId/posts", verifyToken, getUserPosts);
+
+//Update
+router.patch("/:id/like", verifyToken, likePost);
+
+export default router;
